3d42c977801406aeecdedfc1b1a465fbb3bb5bbe
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-12-13 14:30:13 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# Ethan Lawrence
+# Robot game
+# Dec 13 2024
+
+"""
+Plan:
+Build and upgrade your bots
+challange other bots
+equip special predefined and user generated weapons
+"""
+
+# Building
+"""pick combat style (DPS, Tank, Support, | ?Summoner, Cleanup?)
+upgrade combat style
+apply stat-pool (energy)
+apply equipment / abilitys
+confirm construction"""
+
+# Timeslots
+"""pick one of a few challangers | with some info of bot power (Number of bots, cost est)
+Select special event (clone a bot, buff a bot, scrap pile, sell a bot, Theft, bot specific mission {drill part can mine})
+build a bot takes up a timeslot
+day night cycle effects encounter rate & types available
+
+sleep deprication?
+night encounters"""
+
+# Combat
+"""Set battle stakes - scrap, capture robot, Gain time
+Instruct each bot to do something (pokemon doubles)
+other team does random action
+both actions execute based on bot speed
+bots take damage and could break or need repairs
+if hit it high damage cause bot malfunction (lower stats and garbled bot text)"""
